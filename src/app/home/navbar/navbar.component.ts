976a4c8fe2fc562cacacfe0228eead1497825f2d
PedrOrtizM <pedrortizm18_@hotmail.com>
2021-07-16 01:13:18 -0500

    adding fonts, assets and landing page

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public scrollWindow: Subscription | any;
+  @ViewChild('headerNav') headerNav: ElementRef | any;
+
+
+  constructor(private rederer: Renderer2) { }
+
+  public ngOnInit(): void {
+    this.scrollWindow = fromEvent(window, 'scroll').subscribe(() => 
+      this.rederer.addClass(this.headerNav.nativeElement, 'nav-white')
+    );
   }
 
+  ngOnDestroy() {
+    this.scrollWindow.unsubscribe();
+  }
 }
